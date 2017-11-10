6eb359bc5bba04a68174f8e41bcadd42ef46f270
rama <ramakrishna8051@gmail.com>
2017-11-09 18:43:25 -0800

    add on file

--- app/src/androidTest/java/com/example/ramak/myapplication/MainActivityTest1.java
+++ app/src/androidTest/java/com/example/ramak/myapplication/MainActivityTest1.java
@@ -31,10 +31,10 @@ public class MainActivityTest1 {
     private String edit2 = "rk";
     private String edit3 = "rkk";
     Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(RegisterActivity.class.getName(),null,false);
-    Instrumentation.ActivityMonitor monitor1 = getInstrumentation().addMonitor(HomeActivity.class.getName(),null,false);
+    Instrumentation.ActivityMonitor monitor1 = getInstrumentation().addMonitor(HomeActivity1.class.getName(),null,false);
     Instrumentation.ActivityMonitor monitor2 = getInstrumentation().addMonitor(MapLocationActivity.class.getName(),null,false);
     Instrumentation.ActivityMonitor monitor3 = getInstrumentation().addMonitor(ProfileActivity.class.getName(),null,false);
-    Instrumentation.ActivityMonitor monitor4 = getInstrumentation().addMonitor(MainActivity.class.getName(),null,false);
+    Instrumentation.ActivityMonitor monitor4 = getInstrumentation().addMonitor(MessageActivity.class.getName(),null,false);
     Instrumentation.ActivityMonitor monitor5 = getInstrumentation().addMonitor(MapLocationActivity.class.getName(),null,false);
     @Before
     public void setUp() throws Exception {
@@ -95,9 +95,48 @@ public class MainActivityTest1 {
         Espresso.closeSoftKeyboard();
         Espresso.onView(withId(R.id.submit_button)).perform(click());
         Activity home = getInstrumentation().waitForMonitorWithTimeout(monitor1,5000);
-        Espresso.onView(withId(R.id.button2)).perform(click());
+        Espresso.onView(withId(R.id.logout)).perform(click());
         assertNotNull(mActivity);
         home.finish();
+
+    }
+    @Test
+    public void validUserLogintestLaunchOfHomeActivityAndMapLocations(){
+        Espresso.onView(withId(R.id.editTextUserId)).perform(typeText(edit1));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.editTextPassword)).perform(typeText(edit2));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.submit_button)).perform(click());
+        Activity home1 = getInstrumentation().waitForMonitorWithTimeout(monitor2,5000);
+        Espresso.onView(withId(R.id.scan)).perform(click());
+        assertNotNull(home1);
+        home1.finish();
+
+    }
+    @Test
+    public void validUserLogintestLaunchOfHomeActivityAndProfile(){
+        Espresso.onView(withId(R.id.editTextUserId)).perform(typeText(edit1));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.editTextPassword)).perform(typeText(edit2));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.submit_button)).perform(click());
+        Activity home2 = getInstrumentation().waitForMonitorWithTimeout(monitor3,5000);
+        Espresso.onView(withId(R.id.profile)).perform(click());
+        assertNotNull(home2);
+        home2.finish();
+
+    }
+    @Test
+    public void validUserLogintestLaunchOfHomeActivityAndMessage(){
+        Espresso.onView(withId(R.id.editTextUserId)).perform(typeText(edit1));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.editTextPassword)).perform(typeText(edit2));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.submit_button)).perform(click());
+        Activity home3 = getInstrumentation().waitForMonitorWithTimeout(monitor4,5000);
+        Espresso.onView(withId(R.id.message)).perform(click());
+        assertNotNull(home3);
+        home3.finish();
 
     }
     @Test
